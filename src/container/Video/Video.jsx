eae059a7ee604df3bd41c821f21aefe93ec1e838
Morenike Oyewole <herroyalpianist@gmail.com>
2022-09-16 00:03:24 +0100

    fetch tweets data, video and general UI updates

--- src/container/Video/Video.jsx
+++ src/container/Video/Video.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { SubHeading } from "../../components/Headings/Headings";
 import "./Video.scss";
 
-//import Images from "../../constants/images";
+
 
 const Video = () => {
-  return <div>{/* <img src={Images.amala} alt="" /> */}</div>;
+  return (
+    <div className="video">
+      <SubHeading title="How To make amala at home" />
+      <iframe
+        src="https://www.youtube.com/embed/KbYJj_gSZME"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  )
 };
 
 export default Video;
