a3f6d1fbba32a84dbb5be8b554cabe2699a6b8f4
Morenike Oyewole <herroyalpianist@gmail.com>
2022-09-27 11:57:35 +0100

    update map on modal

--- src/Pages/Modal.jsx
+++ src/Pages/Modal.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import { SubHeading } from '../components/Headings/Headings';
 import './Modal.scss';
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import Loader from '../components/Loader';
 
- //name, business_status, geometry.location{}, opening_hours{open_now}, rating, user_ratings_total, vicinity, photos[0].html_attributions
+//name, business_status, geometry.location{}, opening_hours{open_now}, rating, user_ratings_total, vicinity, photos[0].html_attributions
 const Modal = ({ spot, toggleModal }) => {
-console.log(spot);
-    const {isLoaded } = useLoadScript({
+    console.log(spot);
+    const { isLoaded } = useLoadScript({
         googleMapsApiKey: "AIzaSyDCtHM9zzEmpTMx-fS9h6JviXPwyEUNq4c"
     })
 
-    if(!isLoaded){
+    if (!isLoaded) {
         return <Loader />
     }
-    const  center={lat:44, lng:-80}
+    const center = { lat: 44, lng: -80 }
     return (
         <div className="modal">
-            
+
             <div className="modal__dets">
-         <GoogleMap zoom={10} center={center} mapContainerClassName="modal__dets-map"  >
-            <Marker position={center}  />
-         </GoogleMap>
-            
+                <GoogleMap zoom={10} center={center} mapContainerClassName="modal__dets-map"  >
+                    <Marker position={center} />
+                </GoogleMap>
+
                 <SubHeading title={spot.name} />
                 <p>Business Status: <strong>{spot.business_status}</strong> </p>
                 <p>Average Rating: <strong> {spot.rating} out of {spot.user_ratings_total} total Ratings</strong> </p>
                 <p>Address: <strong> {spot.vicinity}</strong></p>
-                <span className="close" onClick={toggleModal}>X</span>
+                <span className="close" onClick={toggleModal}><AiOutlineCloseCircle /></span>
             </div>
         </div>
     )
