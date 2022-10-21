809119657d7bf8fa44bb019cc3c25836eafd12c4
psychokot11 <z.stawarska@gmail.com>
2022-10-21 15:45:14 +0200

    replace img

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -6,9 +6,7 @@ export const BannerSlider = ({ images }) => {
   return (
     <div className="slider">
       <div className="slider__container">
-        {/* <div className="slider__circle--first" /> */}
         <img src={slider1} alt="" />
-        {/* <div className="slider__circle--second" /> */}
       </div>
     </div>
   );
