6c3618b38a029a7e149e26b7668d3d78b2e0d4cb
mo-renike <wilfriedakpe3@gmail.com>
2022-12-27 21:29:10 +0100

    FIX/ set modal details

--- src/Pages/Modal.jsx
+++ src/Pages/Modal.jsx
@@ -8,9 +8,9 @@ import { FaTimes } from "react-icons/fa";
 
 //name, business_status, geometry.location{}, opening_hours{open_now}, rating, user_ratings_total, vicinity, photos[0].html_attributions
 const Modal = ({ spot, toggleModal }) => {
-    // fetch additional data
+    console.log(spot);
 
-    // const [extraData, setExtraData] = useState([]);
+    const [extraData, setExtraData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,94 +18,93 @@ const Modal = ({ spot, toggleModal }) => {
                 extraOptions
             );
             console.log(spotData.data);
-            // setExtraData(spotData.data);
+            setExtraData(spotData.data);
         };
         fetchData();
     }, [spot.business_id]);
 
     return (
-        <h1>hi</h1>
-        // <div className="modal">
-        //     <div className="modal__dets">
-        //         <SubHeading title={spot.name} />
-        //         <div className="modal__photos">
-        //             {/* {extraData.photos &&
-        //                 extraData.photos
-        //                     .slice(0, 9)
-        //                     .map((photo) => (
-        //                         <img
-        //                             src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photo.photo_reference}&key=AIzaSyBiVr3N5E4oa0pBJ8Q8m64UFBk5M0JtdXw`}
-        //                             alt="img"
-        //                         />
-        //                     ))} */}
-        //         </div>
-        //         <SubHeading title="Description:" />
-        //         <p>
-        //             <strong>{spot.name}</strong> is an amala {spot.types[0]} in your area
-        //             that is currently {spot.business_status} and has an average rating of{" "}
-        //             <strong>{spot.rating}</strong> out of{" "}
-        //             <strong>{spot.user_ratings_total} </strong> total ratings. <br />{" "}
-        //             <br />
-        //             They are located at <strong>{extraData.formatted_address}</strong> and
-        //             is currently{" "}
-        //             <strong>
-        //                 {spot.opening_hours.open_now === true ? "Open Now" : "Not Open"}
-        //             </strong>
-        //             . <br /> <br />
-        //             <strong>{spot.name}</strong> can be contacted on <br />
-        //             <strong> {extraData.international_phone_number}</strong> and can be
-        //             found on{" "}
-        //             <strong>
-        //                 <a
-        //                     style={{ color: "blue", textDecoration: "underline" }}
-        //                     href={extraData.website}
-        //                     target="_blank"
-        //                     rel="noopener noreferrer"
-        //                 >
-        //                     their website
-        //                 </a>
-        //             </strong>{" "}
-        //             <br />
-        //         </p>{" "}
-        //         <br />
-        //         <SubHeading title="Opening hours:" />
-        //         {extraData.opening_hours &&
-        //             extraData.opening_hours.weekday_text.map((day) => (
-        //                 <p className="hours">{day}</p>
-        //             ))}
-        //         <SubHeading title="Reviews:" />
-        //         <div className="modal__dets_reviews">
-        //             {extraData.reviews
-        //                 ? extraData.reviews.map((review) => (
-        //                     <div
-        //                         key={review.place_id}
-        //                         className="modal__dets_reviews-review"
-        //                     >
-        //                         <p className="rating"> {review.rating}-star rating</p>
-        //                         <p className="reviewer">
-        //                             <strong>{review.author_name}</strong> -{" "}
-        //                             {review.relative_time_description}
-        //                         </p>
-        //                         <q>
-        //                             <i>{review.text}</i>
-        //                         </q>
-        //                     </div>
-        //                 ))
-        //                 : "No reviews yet"}
-        //         </div>
-        //         <a
-        //             className="button"
-        //             href={`https://www.google.com/maps/search/?api=1&query=${spot.name}&query_place_id=${spot.place_id}`}
-        //             target="_blank"
-        //             rel="noopener noreferrer"
-        //         >
-        //             View on Google Maps
-        //         </a>
-        //         <span className="close" onClick={toggleModal}>
-        //             <FaTimes />
-        //         </span>
-        //     </div>
-        // </div>
+        <div className="modal">
+            <div className="modal__dets">
+                <SubHeading title={spot.name} />
+                <div className="modal__photos">
+                    {/* {extraData.photos &&
+                        extraData.photos
+                            .slice(0, 9)
+                            .map((photo) => (
+                                <img
+                                    src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photo.photo_reference}&key=AIzaSyBiVr3N5E4oa0pBJ8Q8m64UFBk5M0JtdXw`}
+                                    alt="img"
+                                />
+                            ))} */}
+                </div>
+                <SubHeading title="Description:" />
+                <p>
+                    <strong>{spot.name}</strong> is an amala restaurant in your area
+                    that is currently {spot.business_status} and has an average rating of{" "}
+                    <strong>{spot.rating}</strong> out of{" "}
+                    <strong>{spot.user_ratings_total} </strong> total ratings. <br />{" "}
+                    <br />
+                    They are located at <strong>{extraData.formatted_address}</strong> and
+                    is currently{" "}
+                    <strong>
+                        {spot.opening_hours.open_now === true ? "Open Now" : "Not Open"}
+                    </strong>
+                    . <br /> <br />
+                    <strong>{spot.name}</strong> can be contacted on <br />
+                    <strong> {extraData.international_phone_number}</strong> and can be
+                    found on{" "}
+                    <strong>
+                        <a
+                            style={{ color: "blue", textDecoration: "underline" }}
+                            href={extraData.website}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            their website
+                        </a>
+                    </strong>{" "}
+                    <br />
+                </p>{" "}
+                <br />
+                <SubHeading title="Opening hours:" />
+                {spot.working_hours &&
+                    spot.working_hours.weekday_text.map((day) => (
+                        <p className="hours">{day}</p>
+                    ))}
+                <SubHeading title="Reviews:" />
+                <div className="modal__dets_reviews">
+                    {extraData.reviews
+                        ? extraData.reviews.map((review) => (
+                            <div
+                                key={review.place_id}
+                                className="modal__dets_reviews-review"
+                            >
+                                <p className="rating"> {review.rating}-star rating</p>
+                                <p className="reviewer">
+                                    <strong>{review.author_name}</strong> -{" "}
+                                    {review.relative_time_description}
+                                </p>
+                                <q>
+                                    <i>{review.text}</i>
+                                </q>
+                            </div>
+                        ))
+                        : "No reviews yet"}
+                </div>
+                <a
+                    className="button"
+                    href={`https://www.google.com/maps/search/?api=1&query=${spot.name}&query_place_id=${spot.place_id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    View on Google Maps
+                </a>
+                <span className="close" onClick={toggleModal}>
+                    <FaTimes />
+                </span>
+            </div>
+        </div>
     );
 };
 
