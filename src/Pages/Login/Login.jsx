ccf6331fa65b7f11232488e4d03737fb5e82c312
mo-renike <herroyalpianist@gmail.com>
2023-04-03 18:53:20 +0100

    FEAT update Header content according to app state

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import "./Login.scss"
 
-const Login = () => {
-    // build login form with authentication
 
+
+const Login = ({ signIn }) => {
+    // build login form with authentication
 
     return (
         <div className="login">
-           
+            <div className="login__form">
+                <h1>Login to your account</h1>
+                <p>
+                    Save your favourite  spots and share them with your friends.
+                </p>
+                <button onClick={signIn}>Sign in with google</button>
+            </div>
         </div>
     )
 }
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,35 +1,33 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { AppContext } from '../../AppContext';
 import { HashLink } from "react-router-hash-link";
 import BannerSlider from "../../components/BannerSlider/BannerSlider";
 //import { Heading } from "../../components/Headings/Headings";
 import "./Header.scss";
 
-// const title = () => {
-//   return (
-//     <div>
-//       How Many Wraps of <em>Hot, Fluffy Amala</em> Can You Finish?
-//     </div>
-//   );
-// };
 
 const Header = () => {
+  const { foodType } = useContext(AppContext);
+
   return (
     <section className="header" id="home">
       <div className="header__wrapper">
         <div className="header__info">
-          <h1>How Many Wraps of <em>Hot, Fluffy Amala</em> Can You Finish?</h1>
+          {foodType === "amala" ? (<h1>How Many Wraps of <em>Hot, Fluffy Amala</em> Can You Finish?</h1>) :
+            foodType === "chinese" ? (<h1> Find  best the <em>Chinese Cuisine</em> closest to you</h1>) :
+              (<h1>Find the Best <em>Calabar Kitchen</em> Near You - Easy and Quick!</h1>)}
+
           <p className="header__info-text">
-            Your number one stop for everything amala. Find amala spots closest to
+            Find {foodType} spots closest to
             you, check information about each spot including reviews, rating, description, and contact details.
 
           </p>
           <HashLink to="/#spots" className="pry__button">
-            Explore Amala Spots
+            Explore {foodType} Spots
           </HashLink>
         </div>
         <BannerSlider />
       </div>
-
     </section>
   );
 };
