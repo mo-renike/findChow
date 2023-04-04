f1b93ad113a46b39173b72af1df23046021b4b35
mo-renike <herroyalpianist@gmail.com>
2023-04-04 03:47:34 +0100

    Add Profile section on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { MdFavorite, MdLogout, MdOutlineRestaurantMenu } from "react-icons/md";
 import { HashLink } from "react-router-hash-link";
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from "react-places-autocomplete";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import FoodTypeDropdown from "../FoodType/FoodTypeDropdown";
@@ -20,6 +16,7 @@ const Navbar = ({ signOut, user }) => {
     setShow(!show);
   };
 
+
   return (
     <nav className="app__navbar">
       <Link to="/" className="app__navbar-logo">
@@ -28,35 +25,35 @@ const Navbar = ({ signOut, user }) => {
 
       <div className="app__navbar-links">
         <FoodTypeDropdown />
-        <div className="app__navbar-profile">
-          <div onClick={handleShow} className="app__navbar_profile-img">
-            {user && user.photoURL ? (
-              <img src={user.photoURL} alt="" />
-            ) : (
-              <span>{user && user.displayName.slice(0, 2)}</span>
-            )}
-          </div>
+        <div className="app__navbar-user">
+          {!user ? (
+            <Link to="/login" className="app__navbar-link">
+              Login
+            </Link>
+          ) : (
+            <div onClick={handleShow} className="app__navbar-profile">
+              {user &&
+                <div className="app__navbar-profile_flex">
+                  <img src={user.photoURL} alt="" />
+                  <p>{user.displayName}</p>
+                </div>
+              }
+            </div>
+          )}
+
           {show && (
-            <ul className="slide-bottom">
-              <li className="app__navbar-link">
+            <ul className="slide-bottom app__navbar-profile_show">
+              <li>
                 {" "}
-                <Link to="/dashboard" className="app__navbar-link">
-                  Favorites
+                <Link onClick={handleShow} to="/dashboard">
+                  <MdFavorite />  Favorites
                 </Link>
               </li>
 
-              {user ? (
-                <li className="app__navbar-link" onClick={signOut}>
-                  Logout
-                </li>
-              ) : (
-                <li>
-                  {" "}
-                  <Link to="/login" className="app__navbar-link">
-                    Login
-                  </Link>
-                </li>
-              )}
+              <li onClick={signOut}>
+                <MdLogout />    Logout
+              </li>
+
             </ul>
           )}
         </div>
