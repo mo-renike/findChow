b1a8dc5f0606b6f1462ac19555995339ab9429f8
Morenike <herroyalpianist@gmail.com>
2024-08-11 22:24:58 +0100

    implement seo

--- src/components/SEO/DetailsSeo.jsx
+++ src/components/SEO/DetailsSeo.jsx
@@ -13,16 +13,16 @@ const DetailsSeo = ({ spot }) => {
                 name="keywords"
                 content={`restaurant, ${spot.name}, ${spot.vicinity}, food, reviews, chinese cuisine`}
             />
-            <link rel="canonical" href={`https://everything-amala.vercel.app/place/${spot.place_id}`} />
+            <link rel="canonical" href={`https://find-chow.vercel.app/place/${spot.place_id}`} />
             <meta property="og:title" content={`${spot.name} - ${spot.vicinity}`} />
             <meta
                 property="og:description"
                 content={`Explore ${spot.name}, located at ${spot.vicinity}. This spot has a rating of ${spot.rating}/5 from ${spot.user_ratings_total} reviews. Check out more details and reviews.`}
             />
-            <meta property="og:url" content={`https://everything-amala.vercel.app/place/${spot.place_id}`} />
+            <meta property="og:url" content={`https://find-chow.vercel.app/place/${spot.place_id}`} />
             <meta
                 property="og:image"
-                content={spot.icon ? spot.icon : "https://everything-amala.vercel.app/default-image.jpg"}
+                content={spot.icon ? spot.icon : "https://find-chow.vercel.app/default-image.jpg"}
             />
         </Helmet>
     )
--- src/components/SEO/HomeSeo.jsx
+++ src/components/SEO/HomeSeo.jsx
@@ -11,12 +11,12 @@ const HomeSeo = () => {
             />
             <meta name="keywords" content="best amala spot, chinese restaurants near me, calabar restaurant close to me, restaurant in Lagos, nearby places,food reviews, amala spots, chinese restaurant" />
             <meta name="robots" content="index,follow" />
-            <link rel="canonical" href="https://everything-amala.vercel.app/" />
+            <link rel="canonical" href="https://find-chow.vercel.app/" />
             <meta property="og:title" content="Discover Local Restaurants Near You - [Your Web App Name]" />
             <meta property="og:description" content="Find top-rated local dining spots, read reviews, and get the distance to each place from your current location with our web app. Explore amala spots near you today!" />
-            <meta property="og:url" content="https://everything-amala.vercel.app/" />
+            <meta property="og:url" content="https://find-chow.vercel.app/" />
             <meta property="og:type" content="website" />
-            {/* <meta property="og:image" content="https://everything-amala.vercel.app//path-to-image.jpg" /> */}
+            {/* <meta property="og:image" content="https://find-chow.vercel.app//path-to-image.jpg" /> */}
         </Helmet>
     )
 }
